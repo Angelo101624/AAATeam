76999c3b875dd9f18a07fbe0905a88ab70c2889c
Angelo101624 <angelo.dr_94@hotmail.it>
2017-07-14 11:49:12 +0200

    Update Database.java

--- Hello/src/Database.java
+++ Hello/src/Database.java
@@ -4,20 +4,29 @@ import java.util.List;
 
 public class Database {
 	public List<Persona> list_persone = new ArrayList<Persona>();
+	public List<Permesso> list_permessi_propr = new ArrayList<Permesso>();
 	public List<Permesso> list_permessi_dir = new ArrayList<Permesso>();
 	public List<Permesso> list_permessi_dip = new ArrayList<Permesso>();
 	ConnectionMySQL data1=new ConnectionMySQL();
 	
 public Database(){
-	list_persone.add(new Persona("Agostino", "Palmiero","Dipendente"));
-	list_persone.add(new Persona("Antonio", "Divico","Dirigente"));
-	list_persone.add(new Persona("Angelo", "Campomaggiore","Dirigente"));
+	list_persone.add(new Persona("Vincenzo","Vatiero","Proprietario"));	
+	list_persone.add(new Persona("Antonio","Divico","Dirigente"));
+	list_persone.add(new Persona("Angelo","Campomaggiore","Dirigente"));
+	list_persone.add(new Persona("Agostino","Palmiero","Dipendente"));
+	list_persone.add(new Persona("Dario","Perrone","Dipendente"));
+	list_persone.add(new Persona("Giovanni","Salzillo","Dipendente"));
+	
+	list_permessi_propr.add(new Permesso("Registrazione"));
+	list_permessi_propr.add(new Permesso("Accredita stipendio"));
+	
 	list_permessi_dir.add(new Permesso("Statistiche"));
-	list_permessi_dir.add(new Permesso("Magazzino"));
-	list_permessi_dip.add(new Permesso("Badge"));
-	list_permessi_dir.add(new Permesso("Accredito stipendio"));
-	list_permessi_dip.add(new Permesso("Visualizza stipendio"));
+	list_permessi_dir.add(new Permesso("Visualizza stipendio"));
 	list_permessi_dir.add(new Permesso("Contabilita"));
-	list_permessi_dir.add(new Permesso("Registrazione"));
+	list_permessi_dir.add(new Permesso("Badge"));
+	
+	list_permessi_dip.add(new Permesso("Visualizza stipendio"));
+	list_permessi_dip.add(new Permesso("Badge"));
+	list_permessi_dip.add(new Permesso("Magazzino"));
 	}
 }
